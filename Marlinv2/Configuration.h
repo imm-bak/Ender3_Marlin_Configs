04634534e780ef1b4a3846ee1f981d2e8947dbdb
imm-bak <manster45@hotmail.com>
2019-10-07 20:58:07 +1100

    BABYSTEP_ZPROBE_GFX_OVERLAY

--- Marlinv2/Configuration.h
+++ Marlinv2/Configuration.h
@@ -913,7 +913,7 @@
 #define NOZZLE_TO_PROBE_OFFSET { -45, -7, 0 }
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 46
+#define MIN_PROBE_EDGE 20
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
